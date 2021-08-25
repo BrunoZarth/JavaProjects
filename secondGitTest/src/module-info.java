a8826795a6aa586ec598a195061b3720167f8520
brunozarth <brunozarth@gmail.com>
2021-08-25 13:57:56 -0300

    Second hello git test!

---
+++ secondGitTest/src/module-info.java
@@ -0,0 +1,2 @@
+module secondGitTest {
+}
---
+++ secondGitTest/src/secondGitTest/HelloGitAgain.java
@@ -0,0 +1,10 @@
+package secondGitTest;
+
+public class HelloGitAgain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git again!");
+
+	}
+
+}
