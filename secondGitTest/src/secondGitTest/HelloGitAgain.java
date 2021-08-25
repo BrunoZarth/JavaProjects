ad05077627c4b8328ef5cfdcb4ae5ad5da8708b7
brunozarth <brunozarth@gmail.com>
2021-08-25 14:01:42 -0300

    Still testing commands

--- secondGitTest/src/secondGitTest/HelloGitAgain.java
+++ secondGitTest/src/secondGitTest/HelloGitAgain.java
@@ -4,6 +4,10 @@ public class HelloGitAgain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello git again!");
+		
+		for(int i = 0; i <= 100; i++) {
+			System.out.println("This is a simple git test.");
+		}
 
 	}
 
